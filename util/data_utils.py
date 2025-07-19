c6bf27ff8d6b1d2c03113f0087e49e1f49e6dea2
aayushdangol16 <83861312+aayushdangol16@users.noreply.github.com>
2025-07-19 17:09:54 +0545

    Update data_utils.py

--- util/data_utils.py
+++ util/data_utils.py
@@ -130,6 +130,9 @@ def get_random_region_targets(given_layers, graphs, targets):
         graphs_i = graphs[bs_i]
         given_layers_i = given_layers[bs_i]
         sampled_points_i, sampled_edges_i = given_layers_i
+        print(given_layers)
+        print(graphs)
+        print(targets)
 
         if sum(sampled_points_i.values()) == 0:
             random_region_target['edges'] = targets_i['edges'][:1]
